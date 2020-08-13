ca6407a63bc87072e6e0030c1aad419ced8a148b
Paulo Henrique Santos <phsantosferreira9@gmai.com>
2020-08-13 16:33:27 -0300

    Alterações Luciano

---
+++ src/main/java/br/com/rd/pi/pdv/PibootAplication.java
@@ -0,0 +1,14 @@
+package br.com.rd.pi.pdv;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibootAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PibootAplication.class, args);
+    }
+
+}
